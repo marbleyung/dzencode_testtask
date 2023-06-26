fb2f5951f34712739c7e49291ab70ece1f3c6551
Max Shylnikov <raskolbrd@gmail.com>
2023-06-27 01:40:58 +0300

    add signup button

--- frontend/src/components/UI/navbar/MyNavbar.jsx
+++ frontend/src/components/UI/navbar/MyNavbar.jsx
@@ -1,88 +1,49 @@
-  // import React, { useState, useEffect } from "react";
-  // import { Link, useLocation } from "react-router-dom";
-  // import MyButton from "../button/MyButton";
-  // import classes from './MyNavbar.module.css';
-  // import Cookies from 'js-cookie';
+import React, { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
+import MyButton from "../button/MyButton";
+import classes from './MyNavbar.module.css';
+import Cookies from 'js-cookie';
 
-  // const Navbar = () => {
-  //   const [isUserAuth, setIsUserAuth] = useState(null);
-  //   const location = useLocation();
+const Navbar = () => {
+  const [isUserAuth, setIsUserAuth] = useState(null);
+  const location = useLocation();
 
-  //   useEffect(() => {
-  //     const accessToken = Cookies.get('access_token');
-  //     const refreshToken = Cookies.get('refresh_token');
-  //     if (accessToken || refreshToken) {
-  //       setIsUserAuth(true);
-  //     } else {
-  //       setIsUserAuth(false);
-  //     }
-  //   }, []);
-
-  //   const logout = () => {
-  //     Cookies.remove('access_token');
-  //     Cookies.remove('refresh_token');
-  //     setIsUserAuth(false);
-  //     window.location.reload();
-  //   };
-  //   return (
-  //     <div className={classes.navbar}>
-  //       <div className={classes.navbarItems}>
-  //         {location.pathname.endsWith('/login/')
-  //           ? null :
-  //           isUserAuth
-  //             ? <MyButton onClick={logout}>Logout</MyButton>
-  //             : <Link className={classes.navbarLink} to="/login/">Login</Link>
-  //         }
-  //       </div>
-  //     </div>
-  //   )
-  // }
-
-  // export default Navbar;
-  import React, { useState, useEffect } from "react";
-  import { Link, useLocation } from "react-router-dom";
-  import MyButton from "../button/MyButton";
-  import classes from './MyNavbar.module.css';
-  import Cookies from 'js-cookie';
-  
-  const Navbar = () => {
-    const [isUserAuth, setIsUserAuth] = useState(null);
-    const location = useLocation();
-  
-    useEffect(() => {
-      const checkAuthStatus = async () => {
-        const accessToken = Cookies.get('access_token');
-        const refreshToken = Cookies.get('refresh_token');
-        if (accessToken || refreshToken) {
-          setIsUserAuth(true);
-        } else {
-          setIsUserAuth(false);
-        }
-      };
-  
-      checkAuthStatus();
-    }, [location]);
-  
-    const logout = () => {
-      Cookies.remove('access_token');
-      Cookies.remove('refresh_token');
-      setIsUserAuth(false);
-      window.location.reload()
+  useEffect(() => {
+    const checkAuthStatus = async () => {
+      const accessToken = Cookies.get('access_token');
+      const refreshToken = Cookies.get('refresh_token');
+      if (accessToken || refreshToken) {
+        setIsUserAuth(true);
+      } else {
+        setIsUserAuth(false);
+      }
     };
-  
-    return (
-      <div className={classes.navbar}>
-        <div className={classes.navbarItems}>
-          {location.pathname.endsWith('/login/')
-            ? null :
-            isUserAuth
-              ? <MyButton onClick={logout}>Logout</MyButton>
-              : <Link className={classes.navbarLink} to="/login/">Login</Link>
-          }
-        </div>
-      </div>
-    );
+
+    checkAuthStatus();
+  }, [location]);
+
+  const logout = () => {
+    Cookies.remove('access_token');
+    Cookies.remove('refresh_token');
+    setIsUserAuth(false);
+    window.location.reload()
   };
-  
-  export default Navbar;
-  
+
+  return (
+    <div className={classes.navbar}>
+      <div className={classes.navbarItems}>
+        {location.pathname.endsWith('/login/')
+          ? <Link className={classes.navbarLink} to="/signup/">Signup</Link> :
+          isUserAuth
+            ? <MyButton onClick={logout}>Logout</MyButton>
+            : <div>
+              <Link className={classes.navbarLink} to="/login/">Login</Link>
+              <Link className={classes.navbarLink} to="/signup/">Signup</Link>
+            </div>
+        }
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
